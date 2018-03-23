6036024e6d738a098f7005906a5f501603dc33cb
Brad Ballard <brad@bradballard.com>
2018-03-23 11:24:09 -0400

    Clean up method calls

--- app/models/raw_email.rb
+++ app/models/raw_email.rb
@@ -2,67 +2,65 @@ class RawEmail < ApplicationRecord
 
   # Regex to parse email
 
-  CRLF        = /\r\n/
-  WSP         = /[\x9\x20]/
-  FIELD_NAME    = /[\x21-\x39\x3b-\x7e]+/
-  FIELD_BODY    = /.+/m
-  FIELD_SPLIT   = /^(#{FIELD_NAME})\s*:\s*(#{FIELD_BODY})?$/
-  HEADER_SPLIT  = /#{CRLF}(?!#{WSP})/
+  CRLF = /\r\n/
+  WSP = /[\x9\x20]/
+  FIELD_NAME = /[\x21-\x39\x3b-\x7e]+/
+  FIELD_BODY = /.+/m
+  FIELD_SPLIT = /^(#{FIELD_NAME})\s*:\s*(#{FIELD_BODY})?$/
+  HEADER_SPLIT = /#{CRLF}(?!#{WSP})/
   HEADER_BODY_SPLIT = /#{CRLF}#{WSP}*#{CRLF}/m
 
   def self.parse(raw_email)
-    parsed_email = Hash.new
+    parsed_email = {}
 
     fields, body = raw_email.split(HEADER_BODY_SPLIT, 2)
-    body ||= ""
+    body ||= ''
     header_fields = fields.split(HEADER_SPLIT)
-    parsed_email["Header"] = parse_headers(header_fields)
+    parsed_email['Header'] = parse_headers(header_fields)
 
-    if (!parsed_email["Header"]["Content-Type"].nil?)
-      content_type = parsed_email["Header"]["Content-Type"]
+    if !parsed_email['Header']['Content-Type'].nil?
+      content_type = parsed_email['Header']['Content-Type']
     else
-      content_type = "text/plain"
+      content_type = 'text/plain'
     end
 
-    parsed_email["Body"] = parse_body(content_type, body)
+    parsed_email['Body'] = parse_body(content_type, body)
 
-    return parsed_email
+    parsed_email
   end
-
-  private
 
   # Returns a new hash of the header fields
   def self.parse_headers(header_fields)
-    headers = Hash.new
-    if (!header_fields.nil?)
+    headers = {}
+    if !header_fields.nil?
       header_fields.each do |field|
         s = field.split(FIELD_SPLIT)
-        if (!s[1].nil?)
+        if !s[1].nil?
           field_name = s[1]
           headers[field_name] = s[2]
         end
       end
     end
 
-    return headers
+    headers
   end
 
-  def self.parse_body(content_type,raw_body)
+  def self.parse_body(content_type, raw_body)
     mime = parse_mime_type(content_type)
 
-    new_body = String.new
-    if (mime["Type"] == "multipart")
+    new_body = ''
+    if mime['Type'] == 'multipart'
       boundary = parse_boundary(content_type)
       new_body = parse_multipart_body(boundary, raw_body)
     else
       new_body = raw_body
     end
 
-    return new_body
+    new_body
   end
 
   def self.parse_multipart_body(boundary, raw_body)
-    new_body = String.new
+    new_body = ''
 
     parts = raw_body.split(/(:\A|\r\n)--#{boundary}(?=(?:--)?\s*$)/)
 
@@ -72,27 +70,27 @@ class RawEmail < ApplicationRecord
       fields, body = part.split(HEADER_BODY_SPLIT, 2)
 
       # Give a default content type
-      part_content_type = "text/plain"
+      part_content_type = 'text/plain'
 
-      if (!fields.nil?)
+      if !fields.nil?
         headers = parse_headers(fields.split(HEADER_SPLIT))
-        if (!headers["Content-Type"].nil?)
-          part_content_type = headers["Content-Type"]
+        if !headers['Content-Type'].nil?
+          part_content_type = headers['Content-Type']
         end
       end
-      if (!body.nil?)
+      if !body.nil?
         new_body += parse_body(part_content_type, body)
       end
     end
 
-    return new_body
+    new_body
   end
 
   def self.parse_boundary(content_type)
     boundary_equation = content_type.match(/boundary=[\D a-zA-Z0-9]+/)[0]
-    boundary = boundary_equation.split("=")[1]
+    boundary = boundary_equation.split('=')[1]
 
-    return boundary
+    boundary
   end
 
   def self.parse_mime_type(content_type)
@@ -102,6 +100,6 @@ class RawEmail < ApplicationRecord
     type = m[0]
     subtype = m[1]
 
-    return Hash["Type", m[0], "Subtype", m[1]]
+    Hash['Type', m[0], 'Subtype', m[1]]
   end
 end
